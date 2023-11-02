b34d1151fb693f2f6c71528f7c78ef6650f34ab2
Alex Monroy <alexwebpuro@gmail.com>
2023-11-02 06:48:00 -0500

    Initial commit!.

---
+++ alx-pokemon.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * @package           Pokemon
+ * @wordpress-plugin
+ * Plugin Name:       Pokemon
+ * Plugin URI:        https://alexmonroy.com/
+ * Description:       A little test about wordpress development.
+ * Version:           0.1
+ * Requires at least: 5.2
+ * Requires PHP:      8.1
+ * Author:            Alex Monroy
+ * Author URI:        https://alexmonroy.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://alexmonroy.com/
+ * Text Domain:       alx-pokemonS
+ * Domain Path:       /languages
+ */
+
+ 
