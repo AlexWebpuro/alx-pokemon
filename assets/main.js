618b5f127ed030a55d9459c9411639c75b7b6f39
Alex Monroy <alexwebpuro@gmail.com>
2023-11-03 02:30:44 -0500

    Add: Ajax handle.

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,32 @@
-console.log('loaded!');
+function makeAjaxRequest( event ) {
+
+    event.preventDefault();
+    event.target.setAttribute('disabled', '');
+
+    const $alert = document.getElementById('pokedex_old_number');
+    const param = new URLSearchParams( new FormData( document.getElementById('form') ) );
+  
+    fetch(pokemon_vars.ajax_url, {
+      method: 'POST',
+      body: param,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        $alert.style.display = 'block';
+        $alert.innerHTML += data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  
+  // Call the function to make the AJAX request when needed
+  document.querySelector('.pokemon__id').addEventListener( 'click', makeAjaxRequest );
