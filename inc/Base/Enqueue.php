e3117948128aecb2d1a95b1f3e82e0b204a19a8c
Alex Monroy <alexwebpuro@gmail.com>
2023-11-02 22:55:26 -0500

    Add: Pokemon font.

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -11,12 +11,16 @@ class Enqueue extends BaseController {
        add_action('wp_enqueue_scripts', array( $this, 'enqueue' ) );
     }
 
-   public function enqueue() {
-       if( ! is_singular( 'pokemon' ) ) return;
-       $css = $this->plugin_url.'assets/pokemon.css';
-       wp_enqueue_style('alx-pokemon', $css, array(), '', 'all' );
+    public function enqueue() {
+        if( ! is_singular( 'pokemon' ) ) return;
+        $css = $this->plugin_url.'assets/pokemon.css';
+        wp_enqueue_style('alx-pokemon', $css, array(), '1.0', 'all' );
 
-       $bootstrap = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css';
-       wp_enqueue_style('alx-bootstrap', $bootstrap, array(), '', 'all' );
-   }
+        $bootstrap = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css';
+        wp_enqueue_style('alx-bootstrap', $bootstrap, array(), '', 'all' );
+
+        // Pokemon font
+        $pokemon_font = 'https://fonts.cdnfonts.com/css/pokemon-solid';
+        wp_enqueue_style('pokemon', $pokemon_font, array(), '1.0', 'all' );
+    }
 }
